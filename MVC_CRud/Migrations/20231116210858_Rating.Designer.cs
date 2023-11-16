d976d386b9f11f41035b008cb5ce963adbec8a62
artursab <arturs.belavskis@gmail.com>
2023-11-16 23:13:48 +0200

    rating

--- MVC_CRud/Migrations/20231116210858_Rating.Designer.cs
+++ MVC_CRud/Migrations/20231116210858_Rating.Designer.cs
@@ -4,6 +4,7 @@ using MVC_CRud.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVC_CRud.Migrations
 {
     [DbContext(typeof(MVC_CRudContext))]
-    partial class MVC_CRudContextModelSnapshot : ModelSnapshot
+    [Migration("20231116210858_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
